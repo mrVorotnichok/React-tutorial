87567692d3e6bf6dbb37a55c98ce7caac30b16a0
aavyayzya <aavyayzya@developmentontheedge.com>
2017-11-30 17:41:37 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -108,10 +108,11 @@ var News = React.createClass({
 var Add = React.createClass({
   getInitialState: function() { //устанавливаем начальное состояние (state)
     return {
-      btnIsDisabled: true
+      agreeNotChecked: true,
+      authorIsEmpty: true,
+      textIsEmpty: true
     };
   },
-  
   componentDidMount: function() {
     ReactDOM.findDOMNode(this.refs.author).focus();
   },
@@ -119,27 +120,41 @@ var Add = React.createClass({
     e.preventDefault();
     var author = ReactDOM.findDOMNode(this.refs.author).value;
     var text = ReactDOM.findDOMNode(this.refs.text).value;
-
     alert(author + '\n' + text);
   },
-
   onCheckRuleClick: function() {
-    this.setState({btnIsDisabled: !this.state.btnIsDisabled}); //устанавливаем значение в state
+    this.setState({agreeNotChecked: !this.state.agreeNotChecked}); //устанавливаем значение в state
   },
-
+  onAuthorChange: function(e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({authorIsEmpty: false})
+    } else {
+      this.setState({authorIsEmpty: true})
+    }
+  },
+  onTextChange: function(e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({textIsEmpty: false})
+    } else {
+      this.setState({textIsEmpty: true})
+    }
+  },
   render: function() {
+    var agreeNotChecked = this.state.agreeNotChecked,
+      authorIsEmpty = this.state.authorIsEmpty,
+      textIsEmpty = this.state.textIsEmpty;
     return (
       <form className='add cf'>
         <input
           type='text'
           className='add__author'
-          defaultValue=''
+          onChange={this.onAuthorChange}
           placeholder='Ваше имя'
           ref='author'
         />
         <textarea
           className='add__text'
-          defaultValue=''
+          onChange={this.onTextChange}
           placeholder='Текст новости'
           ref='text'
         ></textarea>
@@ -147,12 +162,11 @@ var Add = React.createClass({
           <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с правилами
         </label>
 
-        {/* берем значение для disabled атрибута из state */}
         <button
           className='add__btn'
           onClick={this.onBtnClickHandler}
           ref='alert_button'
-          disabled={this.state.btnIsDisabled}
+          disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
         >
           Показать alert
         </button>

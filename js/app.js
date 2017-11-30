2c4dba7f6e053c4931e6a562c7c8e1a7d630747f
aavyayzya <aavyayzya@developmentontheedge.com>
2017-11-30 17:16:09 +0700

    add form

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 var myNews = [
   {
     author: 'first author',
-    text: 'fist new'
+    title: 'fist new',
+    text: 'description new'
   },
 
   {
     author: 'second author',
-    text: 'second new'
+    title: 'second new',
+    text: 'description second new'
   },
 ];
 
@@ -14,18 +16,43 @@ var Article = React.createClass({
   propTypes: {
     data: React.PropTypes.shape({
       author: React.PropTypes.string.isRequired,
+      title: React.PropTypes.string.isRequired,
       text: React.PropTypes.string.isRequired
     })
   },
 
+  getInitialState: function () {
+    return {
+      visible: false
+    }
+  },
+
+  /*функция для изменения состояния компонента*/
+  readMoreClick : function (e) {
+    e.preventDefault();
+    this.setState({visible: true})
+  },
+
   render: function () {
     var author = this.props.data.author,
-        text = this.props.data.author;
+        title = this.props.data.title,
+        text = this.props.data.text,
+        visible = this.state.visible; // считываем значение переменной из состояния компонента
 
     return (
-      <div className="article">
-        <p className="news__author">{author}:</p>
-        <p className="news__text">{text}</p>
+      <div className='article'>
+        <p className='news__author'>{author}:</p>
+        <p className='news__title'>{title}</p>
+
+        {/* для ссылки readmore: не показывай ссылку, если visible === true */}
+        <a href="#"
+           onClick={this.readMoreClick}
+           className={'news__readMore ' + (visible ? 'none':'')}>
+          Подробнее
+        </a>
+
+        {/*не показывай текст, если visible === false */}
+        <p className={'news__text ' + (visible ? '':'none')}>{text}</p>
       </div>
     )
   }
@@ -34,6 +61,16 @@ var Article = React.createClass({
 var News = React.createClass({
   propsTypes: {
     data: React.PropTypes.array.isRequired
+  },
+
+  getInitialState: function () {
+    return {
+      counter: 0
+    }
+  },
+
+  onTotalNewsClick: function() {
+    this.setState({counter: ++this.state.counter });
   },
 
   render: function () {
@@ -58,9 +95,62 @@ var News = React.createClass({
     return(
       <div className="news">
         {newsTemplate}
-        <strong className={data.length > 0 ? '':'.none'}>Всего	новостей:	{data.length}</strong>
+        <strong
+          className={data.length > 0 ? '':'.none'}
+          onClick={this.onTotalNewsClick}>
+          Всего	новостей:	{data.length}
+        </strong>
       </div>
     )
+  }
+});
+
+var Add = React.createClass({
+  getInitialState: function() { //устанавливаем начальное состояние (state)
+    return {
+      btnIsDisabled: true
+    };
+  },
+  componentDidMount: function() {
+    ReactDOM.findDOMNode(this.refs.author).focus();
+  },
+  onBtnClickHandler: function(e) {
+    e.preventDefault();
+  },
+  onCheckRuleClick: function() {
+    this.setState({btnIsDisabled: !this.state.btnIsDisabled}); //устанавливаем значение в state
+  },
+  render: function() {
+    return (
+      <form className='add cf'>
+        <input
+          type='text'
+          className='add__author'
+          defaultValue=''
+          placeholder='Ваше имя'
+          ref='author'
+        />
+        <textarea
+          className='add__text'
+          defaultValue=''
+          placeholder='Текст новости'
+          ref='text'
+        ></textarea>
+        <label className='add__checkrule'>
+          <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с правилами
+        </label>
+
+        {/* берем значение для disabled атрибута из state */}
+        <button
+          className='add__btn'
+          onClick={this.onBtnClickHandler}
+          ref='alert_button'
+          disabled={this.state.btnIsDisabled}
+        >
+          Показать alert
+        </button>
+      </form>
+    );
   }
 });
 
@@ -68,6 +158,7 @@ var App = React.createClass({
   render: function() {
     return (
       <div className="app">
+        <Add/>
         <h3>Новости</h3>
         <News data={myNews}/>
       </div>
